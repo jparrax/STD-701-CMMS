73fb13c0f3484afa4f9a3ae15e9ec2b91a721aa4
Pangseen Puarat <pangseen.puarat@xencio.at>
2020-03-29 18:34:37 +1300

    Disabled the security of the system temporarily

--- src/main/java/std701/cmms/api/security/CustomUserDetailsService.java
+++ src/main/java/std701/cmms/api/security/CustomUserDetailsService.java
@@ -1,15 +1,12 @@
 package std701.cmms.api.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import std701.cmms.api.models.User;
 import std701.cmms.api.repositories.UserRepository;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService {
+//public class CustomUserDetailsService implements UserDetailsService {
 
     private UserRepository userRepository;
 
@@ -18,13 +15,13 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        final User appUser = userRepository.findByUsername(username);
-        if (appUser == null) {
-            throw new UsernameNotFoundException(username);
-        }
-
-        return null;
-    }
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        final User appUser = userRepository.findByUsername(username);
+//        if (appUser == null) {
+//            throw new UsernameNotFoundException(username);
+//        }
+//
+//        return null;
+//    }
 }
