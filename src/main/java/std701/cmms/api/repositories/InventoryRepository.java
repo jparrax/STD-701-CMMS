a950dd6d01cab8b9c13a513559f883e543810962
Pangseen Puarat <pangseen.puarat@xencio.at>
2020-03-30 00:34:50 +1300

    Added new method to find inventories by a raw material

--- src/main/java/std701/cmms/api/repositories/InventoryRepository.java
+++ src/main/java/std701/cmms/api/repositories/InventoryRepository.java
@@ -2,6 +2,10 @@ package std701.cmms.api.repositories;
 
 import std701.cmms.api.models.Inventory;
 import org.springframework.data.repository.CrudRepository;
+import std701.cmms.api.models.RawMaterial;
+
+import java.util.List;
 
 public interface InventoryRepository extends CrudRepository<Inventory, Integer> {
+    public List<Inventory> findByRawMaterialOrderByCreatedAtDesc(RawMaterial rawMaterial);
 }
